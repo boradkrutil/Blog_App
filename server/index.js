cf24344f1ca3042cc734bffb86f9153a5645782b
boradkrutil <krutilborad2020@gmail.com>
2024-10-04 05:30:56 +0530

    change file name

--- server/index.js
+++ server/index.js
@@ -24,6 +24,9 @@ const PORT = process.env.PORT || 5000 ;
 
 app.use(express.static(path.join(__dirname , "public") ))
 
+app.use("/",(req,res)=>{
+    res.send("<center><h1>Backend Server is starting...</h1></center>")
+})
 const server = app.listen(PORT,()=>{
 
     console.log(`Server running on port  ${PORT} : ${process.env.NODE_ENV}`)
